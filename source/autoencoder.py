da6fad9fb9386b5e95fc4831643a24ba1bd33966
Paul A. Beata <pbeata@users.noreply.github.com>
2022-07-26 11:54:34 -0400

    add methods to save and reload autoencoder model

--- source/autoencoder.py
+++ source/autoencoder.py
@@ -1,10 +1,13 @@
 import numpy as np
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, \
-									Flatten, Dense, Reshape, Conv2DTranspose, Activation
+                                    Flatten, Dense, Reshape, Conv2DTranspose, Activation
 from tensorflow.keras import backend as K
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import MeanSquaredError
+
+import os
+import pickle
 
 
 class Autoencoder:
@@ -52,7 +55,47 @@ class Autoencoder:
                        batch_size=batch_size,
                        epochs=num_epochs,
                        shuffle=True)
-        
+
+    def save(self, save_folder="."):
+        self._create_folder_if_needed(save_folder)
+        self._save_parameters(save_folder)
+        self._save_weights(save_folder)
+
+    def load_weights(self, weights_path):
+        self.model.load_weights(weights_path)
+
+    @classmethod
+    def load(cls, save_folder="."):
+        parameters_path = os.path.join(save_folder, "parameters.pkl")
+        with open(parameters_path, "rb") as f:
+            parameters = pickle.load(f)
+        autoencoder = Autoencoder(*parameters)
+        weights_path = os.path.join(save_folder, "weights.h5")
+        autoencoder.load_weights(weights_path)
+        return autoencoder
+
+    def _create_folder_if_needed(self, folder):
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    def _save_parameters(self, folder):
+        parameters = [
+            self.input_shape,
+            self.conv_filters,
+            self.conv_kernels,
+            self.conv_strides,
+            self.latent_space_dim
+        ]
+
+        save_path = os.path.join(folder, "parameters.pkl")
+        with open(save_path, "wb") as f:
+            pickle.dump(parameters, f)
+
+    def _save_weights(self, folder):
+        save_path = os.path.join(folder, "weights.h5")
+        self.model.save_weights(save_path)
+
+
     def _build(self):
         self._build_encoder()
         self._build_decoder()
@@ -171,12 +214,12 @@ class Autoencoder:
 
 
 if __name__ == "__main__":
-	# example of how to create an Autoencoder instance and print the summary
-	autoencoder = Autoencoder(
-	    input_shape=(28, 28, 1),
-	    conv_filters=(32, 64, 64, 64),
-	    conv_kernels=(3, 3, 3, 3),
-	    conv_strides=(1, 2, 2, 1),
-	    latent_space_dim=2
-	)
-	autoencoder.summary()
+    # example of how to create an Autoencoder instance and print the summary
+    autoencoder = Autoencoder(
+        input_shape=(28, 28, 1),
+        conv_filters=(32, 64, 64, 64),
+        conv_kernels=(3, 3, 3, 3),
+        conv_strides=(1, 2, 2, 1),
+        latent_space_dim=2
+    )
+    autoencoder.summary()
